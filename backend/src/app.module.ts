92110ff0d69a564701949b29c270aad51248ec6b
Thibaut Gontier <thibautgontier@Thibauts-MacBook-Pro-2.local>
2022-06-29 16:47:04 +0200

    simple auth using sessions

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,19 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { configValidationSchema } from './config.schema';
+import { LoginModule } from './login/login.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: './../docker/backend/.env',
+      validationSchema: configValidationSchema,
+    }),
+    LoginModule,
+    HttpModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+import { HttpExceptionFilter } from './http-exception.filter';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import * as passport from 'passport';
+import { allow } from 'joi';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
+
+  app.use(
+    session({ resave: false, saveUninitialized: false, secret: '!Seoul' }),
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
   await app.listen(3000);
 }
 bootstrap();
